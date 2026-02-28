679ac5e09aa88c041f54a8d0d8f6648a6ab77179
Aranghat Tech <aranghat@outlook.com>
2026-02-28 16:07:12 +0530

    Added Version Description

--- ReactDemoProject/src/App.tsx
+++ ReactDemoProject/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <h1 className="text-4xl font-bold">This is a sample Web Page made in Tailwind</h1>
+        <p>This is a new change that is added on version 2.0</p>
     </div>
   )
 }
