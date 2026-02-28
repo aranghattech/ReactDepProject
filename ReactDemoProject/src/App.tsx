53c24ce9e34135316367a08ca5f6bcc168fd72e8
Aranghat Tech <aranghat@outlook.com>
2026-02-28 15:49:31 +0530

    Initial Commit

---
+++ ReactDemoProject/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-4xl font-bold">This is a sample Web Page made in Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
